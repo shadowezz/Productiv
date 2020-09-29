368c23c29a3a63ceea181b0258dce092dfdc5d74
Clara <claraadora@gmail.com>
2020-09-30 02:00:56 +0800

    Add (temporary) storage files for meeting

---
+++ src/main/java/seedu/address/storage/StorageMeeting.java
@@ -0,0 +1,35 @@
+package seedu.address.storage;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.model.person.ReadOnlyAddressBook;
+import seedu.address.model.person.ReadOnlyUserPrefs;
+import seedu.address.model.person.UserPrefs;
+import seedu.address.storage.person.AddressBookStorage;
+import seedu.address.storage.person.UserPrefsStorage;
+
+/**
+ * API of the Storage component
+ */
+// TODO: build and change to MeetingBookStorage
+public interface StorageMeeting extends AddressBookStorage, UserPrefsStorage {
+
+    @Override
+    Optional<UserPrefs> readUserPrefs() throws DataConversionException, IOException;
+
+    @Override
+    void saveUserPrefs(ReadOnlyUserPrefs userPrefs) throws IOException;
+
+    @Override
+    Path getAddressBookFilePath();
+
+    @Override
+    Optional<ReadOnlyAddressBook> readAddressBook() throws DataConversionException, IOException;
+
+    @Override
+    void saveAddressBook(ReadOnlyAddressBook addressBook) throws IOException;
+
+}
