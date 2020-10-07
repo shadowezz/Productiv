32be36db253499ee6ae291f8f8c21edda0cc078e
Clara <claraadora@gmail.com>
2020-10-07 22:46:13 +0800

    Add userpref methods and meetingBookParser

--- src/main/java/seedu/address/logic/LogicMeetingManager.java
+++ src/main/java/seedu/address/logic/LogicMeetingManager.java
@@ -1,7 +1,11 @@
 package seedu.address.logic;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.logging.Logger;
 
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -9,19 +13,20 @@ import seedu.address.logic.commands.meeting.Command;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.logic.parser.meeting.MeetingBookParser;
 import seedu.address.model.meeting.ModelMeeting;
+import seedu.address.model.meeting.ReadOnlyMeetingBook;
+import seedu.address.model.meeting.meeting.Meeting;
 import seedu.address.storage.meeting.StorageMeeting;
 
 /**
  * Manages the logic for the meeting feature.
  */
 public class LogicMeetingManager implements LogicMeeting {
-    // public static final String FILE_OPS_ERROR_MESSAGE = "Could not save data to file: ";
-    private final Logger logger = LogsCenter.getLogger(LogicMeetingManager.class);
 
+    public static final String FILE_OPS_ERROR_MESSAGE = "Could not save data to meeting file: ";
+    private final Logger logger = LogsCenter.getLogger(LogicMeetingManager.class);
     private final ModelMeeting modelMeeting;
     private final StorageMeeting storageMeeting;
-    private final MeetingBookParser meetingParser;
-    // TODO: Add storage
+    private final MeetingBookParser meetingBookParser;
 
     /**
      * Constructs a {@code LogicMeetingManager} with the given {@code ModelMeeting} and {@code Storage}.
@@ -29,7 +34,7 @@ public class LogicMeetingManager implements LogicMeeting {
     public LogicMeetingManager(ModelMeeting modelMeeting, StorageMeeting storageMeeting) {
         this.modelMeeting = modelMeeting;
         this.storageMeeting = storageMeeting;
-        this.meetingParser = new MeetingBookParser();
+        this.meetingBookParser = new MeetingBookParser();
     }
 
     @Override
@@ -37,20 +42,41 @@ public class LogicMeetingManager implements LogicMeeting {
         logger.info("----------------[USER COMMAND][" + commandText + "]");
 
         CommandResult commandResult;
-        Command command = meetingParser.parseCommand(commandText);
+        Command command = meetingBookParser.parseCommand(commandText);
         commandResult = command.execute(modelMeeting);
 
-        //try {
-        //    storageMeeting.saveAddressBook(modelMeeting.getMeetingBook());
-        // } catch (IOException ioe) {
-        //    throw new CommandException(FILE_OPS_ERROR_MESSAGE + ioe, ioe);
-        // }
+        try {
+            storageMeeting.saveMeetingBook(modelMeeting.getMeetingBook());
+         } catch (IOException ioe) {
+            throw new CommandException(FILE_OPS_ERROR_MESSAGE + ioe, ioe);
+         }
 
         return commandResult;
     }
 
-    public boolean isMeetingCommand(String commandText) {
-        return meetingParser.isMeetingCommand(commandText);
+    @Override
+    public ReadOnlyMeetingBook getMeetingBook() {
+        return modelMeeting.getMeetingBook();
+    }
+
+    @Override
+    public ObservableList<Meeting> getFilteredMeetingList() {
+        return modelMeeting.getFilteredMeetingList();
+    }
+
+    @Override
+    public Path getMeetingBookFilePath() {
+        return modelMeeting.getMeetingBookFilePath();
+    }
+
+    @Override
+    public GuiSettings getGuiSettings() {
+        return modelMeeting.getGuiSettings();
+    }
+
+    @Override
+    public void setGuiSettings(GuiSettings guiSettings) {
+        modelMeeting.setGuiSettings(guiSettings);
     }
 
 }
