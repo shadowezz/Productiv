b1d0dacabbd58701120538fd9fe8e8c28d604e27
Clara <claraadora@gmail.com>
2020-10-07 14:14:24 +0800

    Add Description class for Meeting

--- src/main/java/seedu/address/model/meeting/meeting/Description.java
+++ src/main/java/seedu/address/model/meeting/meeting/Description.java
@@ -1,4 +1,56 @@
 package seedu.address.model.meeting.meeting;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Meeting's description in the meeting book.
+ */
 public class Description {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Descriptions should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the description must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+
+    public final String value;
+
+    /**
+     * Constructs a {@code Description}.
+     *
+     * @param description A valid description.
+     */
+    public Description(String description) {
+        requireNonNull(description);
+        checkArgument(isValidDescription(description), MESSAGE_CONSTRAINTS);
+        value = description;
+    }
+
+    /**
+     * Returns true if a given string is a valid description.
+     */
+    public static boolean isValidDescription(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof Description
+                && value.equals(((Description) other).value));
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
 }
