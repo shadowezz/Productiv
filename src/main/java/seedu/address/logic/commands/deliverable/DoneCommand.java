10b21e30528c35c507d7b4eb5552020b0ad21dfa
shadowezz <wenjie.cao@hotmail.com>
2020-10-09 14:36:10 +0800

    Implement execute method in DoneCommand

--- src/main/java/seedu/address/logic/commands/deliverable/DoneCommand.java
+++ src/main/java/seedu/address/logic/commands/deliverable/DoneCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands.deliverable;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.deliverable.ModelDeliverable.PREDICATE_SHOW_ALL_DELIVERABLES;
 
 import java.util.List;
 
@@ -9,7 +10,10 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.deliverable.ModelDeliverable;
+import seedu.address.model.deliverable.deliverable.Deadline;
 import seedu.address.model.deliverable.deliverable.Deliverable;
+import seedu.address.model.util.Description;
+import seedu.address.model.util.Title;
 
 /**
  * Completes a deliverable
@@ -17,6 +21,7 @@ import seedu.address.model.deliverable.deliverable.Deliverable;
 public class DoneCommand extends Command {
 
     public static final String COMMAND_WORD = "done";
+    private static final boolean COMPLETED = true;
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Marks as done the deliverable identified by the index number used in the displayed deliverable list\n"
             + "Parameters: INDEX (must be a positive integer)\n"
@@ -26,13 +31,14 @@ public class DoneCommand extends Command {
 
     private final Index targetIndex;
 
+
     public DoneCommand(Index targetIndex) {
+        requireNonNull(targetIndex);
         this.targetIndex = targetIndex;
     }
 
     @Override
     public CommandResult execute(ModelDeliverable modelDeliverable) throws CommandException {
-        requireNonNull(modelDeliverable);
         requireNonNull(modelDeliverable);
         List<Deliverable> lastShownList = modelDeliverable.getFilteredDeliverableList();
 
@@ -41,7 +47,19 @@ public class DoneCommand extends Command {
         }
 
         Deliverable deliverableToComplete = lastShownList.get(targetIndex.getZeroBased());
-        modelDeliverable.completeDeliverable(deliverableToComplete);
+        Deliverable completedDeliverable = createCompletedDeliverable(deliverableToComplete);
+        modelDeliverable.setDeliverable(deliverableToComplete, completedDeliverable);
+        modelDeliverable.updateFilteredDeliverableList(PREDICATE_SHOW_ALL_DELIVERABLES);
         return new CommandResult(String.format(MESSAGE_DONE_DELIVERABLE_SUCCESS, deliverableToComplete));
     }
+
+    private Deliverable createCompletedDeliverable(Deliverable deliverableToComplete) {
+        Title title = deliverableToComplete.getTitle();
+        Description description = deliverableToComplete.getDescription();
+        Deadline deadline = deliverableToComplete.getDeadline();
+        String contacts = deliverableToComplete.getContacts();
+        return new Deliverable(title, description, deadline, COMPLETED, contacts);
+    }
+
+
 }
