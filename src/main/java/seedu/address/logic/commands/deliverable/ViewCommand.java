af8daee53771998d0adad6ce9c8ad5bb375705ce
shadowezz <wenjie.cao@hotmail.com>
2020-10-16 15:45:50 +0800

    Implement view command.

---
+++ src/main/java/seedu/address/logic/commands/deliverable/ViewCommand.java
@@ -0,0 +1,49 @@
+package seedu.address.logic.commands.deliverable;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.deliverable.ModelDeliverable;
+import seedu.address.model.deliverable.deliverable.Deliverable;
+
+/**
+ * View a deliverable by displaying its details in the side panel
+ */
+public class ViewCommand extends Command {
+    public static final String COMMAND_WORD = "view";
+    public static final String MESSAGE_VIEW_DELIVERABLE_SUCCESS = "Viewing deliverable: %1$s";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Views the details of the deliverable identified by the index number used in the displayed deliverable list\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    private final Index targetIndex;
+
+    /**
+     * Construct command given index of deliverable to complete.
+     * @param targetIndex specified index of deliverable to complete.
+     */
+    public ViewCommand(Index targetIndex) {
+        requireNonNull(targetIndex);
+        this.targetIndex = targetIndex;
+    }
+
+
+    @Override
+    public CommandResult execute(ModelDeliverable modelDeliverable) throws CommandException {
+        requireNonNull(modelDeliverable);
+        List<Deliverable> lastShownList = modelDeliverable.getFilteredDeliverableList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_DELIVERABLE_DISPLAYED_INDEX);
+        }
+
+        Deliverable deliverableToView = lastShownList.get(targetIndex.getZeroBased());
+        return new CommandResult(String.format(MESSAGE_VIEW_DELIVERABLE_SUCCESS, deliverableToView));
+    }
+}
