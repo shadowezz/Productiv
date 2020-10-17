1ea58ee179dcad71fbfc84b0fef704682ae8eed0
MerlinLim <merlinlim98@gmail.com>
2020-10-17 16:52:03 +0800

    Implement DateTime for fields To and From
    To and From extends from DateTime.

--- src/main/java/seedu/address/model/meeting/meeting/To.java
+++ src/main/java/seedu/address/model/meeting/meeting/To.java
@@ -1,65 +1,26 @@
 package seedu.address.model.meeting.meeting;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import seedu.address.model.util.DateTime;
 
 /**
  * Represents a Meeting's to in the meeting book.
  */
-public class To {
-    public static final String MESSAGE_CONSTRAINTS =
-            "Tos should be in the format of MM/DD/YYYY or MM/DD/YYYY HH:mm, and should not be blank."
-                    + "Note: Single digit month, day, and minute can start with a leading zero.";
-
-    /*
-     * Format should be MM/DD/YYYY or MM/DD/YY HH:mm
-     * Single digit month, day, and minute can start with a leading zero.
-     * Solution below adapted from https://stackoverflow.com/a/51231
-     */
-    // TODO: change back
-    // public static final String VALIDATION_REGEX = "[0-9]{2}/[0-9]{2}/[0-9]{4} [0-23]{2}:[0-59]{2}";
-    public static final String VALIDATION_REGEX = "[^\\s].*";
-
-    public final Date value;
-
-    public final String valueString;
+public class To extends DateTime {
 
     /**
      * Constructs a {@code To}.
      *
      * @param to A valid To.
      */
-
-    //    // Todo: Discuss with team
-    //    public To(String to) throws ParseException {
-    //        requireNonNull(to);
-    //        checkArgument(isValidTo(to), MESSAGE_CONSTRAINTS);
-    //        valueString = to;
-    //        value = DateParser.parseDate(to);
-    //    }
-
-    // Todo: Discuss with team
     public To(String to) {
-        requireNonNull(to);
-        checkArgument(isValidTo(to), MESSAGE_CONSTRAINTS);
-        valueString = to;
-        value = new Date();
+        super(to);
     }
 
     /**
      * Returns true if a given string is a valid To.
      */
     public static boolean isValidTo(String test) {
-        return test.matches(VALIDATION_REGEX);
-    }
-
-    // TODO: abstract?
-    @Override
-    public String toString() {
-        return new SimpleDateFormat("MMM d yyyy").format(value);
+        return isValidDateTime(test);
     }
 
     @Override
@@ -67,11 +28,6 @@ public class To {
         return other == this
                 || (other instanceof To
                 && value.equals(((To) other).value));
-    }
-
-    @Override
-    public int hashCode() {
-        return value.hashCode();
     }
 
 }
