a1e51b0a3c56135c1645ff4edafa203ed243d5e6
MerlinLim <merlinlim98@gmail.com>
2020-10-21 13:21:30 +0800

    Remove optional feature
    DateTime takes a compulsory string.
    Remove Optionals.

--- src/main/java/seedu/address/model/util/DateTime.java
+++ src/main/java/seedu/address/model/util/DateTime.java
@@ -3,11 +3,8 @@ package seedu.address.model.util;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
-import java.util.Optional;
 
 public class DateTime implements Comparable<DateTime> {
     public static final String TIME_REGEX = "(([0-1]\\d)|(2[0-3])):([0-5]\\d)";
@@ -17,16 +14,13 @@ public class DateTime implements Comparable<DateTime> {
                     + "and should not be blank. Note: Single digit month, day, and "
                     + "minute must start with a leading zero.";
 
-    public static final String VALIDATION_REGEX = String.format("%s(\\s(%s))?",
+    public static final String VALIDATION_REGEX = String.format("%s(\\s(%s))",
             DATE_REGEX, TIME_REGEX);
 
     public static final DateTimeFormatter DATE_TIME_FORMATTER =
-            DateTimeFormatter.ofPattern("dd-MM-yyyy[ HH:mm]");
+            DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
-    public static final String EMPTY_DATETIME_VALUE_STRING = "-";
-
-    public final Optional<LocalDateTime> value;
-    public final String valueString;
+    public final LocalDateTime value;
 
     /**
      * Constructs a {@code date}.
@@ -39,51 +33,8 @@ public class DateTime implements Comparable<DateTime> {
         // Check for constraints
         checkArgument(isValidDateTime(date), MESSAGE_CONSTRAINTS);
 
-        // Parse value
-        TemporalAccessor temporalAccessor = DATE_TIME_FORMATTER.parseBest(date,
-                LocalDateTime::from, LocalDate::from);
-        if (temporalAccessor instanceof LocalDateTime) {
-            LocalDateTime dateTimeParsed = (LocalDateTime) temporalAccessor;
-            this.value = Optional.of(dateTimeParsed);
-            this.valueString = dateTimeParsed.format(DATE_TIME_FORMATTER);
-        } else {
-            LocalDate dateParsed = (LocalDate) temporalAccessor;
-            this.value = Optional.of((dateParsed).atStartOfDay());
-            this.valueString = dateParsed.format(DATE_TIME_FORMATTER);
-        }
-
-    }
-
-    /**
-     * Constructs a {@code date}.
-     *
-     * @param date A valid optional DateTime.
-     */
-    public DateTime(Optional<String> date) {
-        // Check for constraints
-        date.ifPresent(d -> checkArgument(isValidDateTime(d), MESSAGE_CONSTRAINTS));
-
         //Parse value
-        Optional<TemporalAccessor> temporalAccessor = date.map(d -> DATE_TIME_FORMATTER
-                .parseBest(d, LocalDateTime::from, LocalDate::from));
-        Optional<LocalDateTime> dateTimeParsed = temporalAccessor
-                .filter(t -> t instanceof LocalDateTime)
-                .map(t -> ((LocalDateTime) t));
-        Optional<LocalDate> dateParsed = temporalAccessor
-                .filter(t -> t instanceof LocalDate)
-                .map(t -> ((LocalDate) t));
-
-        if (dateTimeParsed.isPresent()) {
-            this.value = dateTimeParsed;
-            this.valueString = dateTimeParsed
-                    .map(dt -> dt.format(DATE_TIME_FORMATTER))
-                    .orElse(EMPTY_DATETIME_VALUE_STRING);
-        } else {
-            this.value = dateParsed.map(LocalDate::atStartOfDay);
-            this.valueString = dateParsed
-                    .map(dt -> dt.format(DATE_TIME_FORMATTER))
-                    .orElse(EMPTY_DATETIME_VALUE_STRING);
-        }
+        this.value = LocalDateTime.parse(date, DATE_TIME_FORMATTER);
 
     }
 
@@ -97,41 +48,21 @@ public class DateTime implements Comparable<DateTime> {
         return test.matches(VALIDATION_REGEX);
     }
 
-    /**
-     * Checks is DateTime exists.
-     *
-     * @return true if value in DateTime exists.
-     */
-    public boolean isEmpty() {
-        return this.value.isEmpty();
-    }
-
     @Override
     public int compareTo(DateTime o) {
-        if (value.isEmpty() && o.isEmpty()) {
-            return 0;
-        } else {
-            return this.value.map(v1 ->
-                    o.value.map(v2-> v1.compareTo(v2)).orElse(-1))
-                    .orElse(1);
-        }
-
+        return this.value.compareTo(o.value);
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof DateTime) // instanceof handles nulls
-                && ((value.isEmpty() && ((DateTime) other).value.isEmpty()) //if v1 and v2 empty, return true
-                || value.map(v1 -> ((DateTime) other)
-                .value.map(v2-> v1.equals(v2))
-                .orElse(false)) // if v2 not present, return false
-                .orElse(false)); // if v1 not present return false
+                && ((this.value.equals(((DateTime) other).value)));
     }
 
     @Override
     public String toString() {
-        return valueString;
+        return this.value.format(DATE_TIME_FORMATTER);
     }
 
     @Override
