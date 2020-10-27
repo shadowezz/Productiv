d0d4a71debec1fd459ee71c703e9d2a20de69603
Clara <claraadora@gmail.com>
2020-10-27 11:55:02 +0800

    Update comparator to take in TimeEvent

--- src/main/java/seedu/address/model/meeting/util/TimeEventComparator.java
+++ src/main/java/seedu/address/model/meeting/util/TimeEventComparator.java
@@ -3,19 +3,19 @@ package seedu.address.model.meeting.util;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 
-import seedu.address.model.meeting.meeting.Meeting;
+import seedu.address.model.event.TimeEvent;
 
 /**
  * Represents a Comparator for Meeting which sorts by From.
  */
-public class MeetingComparator implements Comparator<Meeting> {
+public class TimeEventComparator implements Comparator<TimeEvent> {
     /**
      * Compares Meetings by From in ascending order.
      */
-    public int compare(Meeting a, Meeting b) {
-        LocalDateTime aFrom = a.getFromLocalDateTime();
-        LocalDateTime bFrom = b.getFromLocalDateTime();
+    public int compare(TimeEvent a, TimeEvent b) {
+        LocalDateTime timeA = a.getIndicatorTime();
+        LocalDateTime timeB = b.getIndicatorTime();
 
-        return aFrom.compareTo(bFrom);
+        return timeA.compareTo(timeB);
     }
 }
