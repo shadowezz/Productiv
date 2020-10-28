eca1f5e907b4cd5294ef7fb22d29801990fdc934
shadowezz <wenjie.cao@hotmail.com>
2020-10-28 11:45:34 +0800

    Implement find command for deliverable.

---
+++ src/main/java/seedu/address/logic/parser/deliverable/FindCommandParser.java
@@ -0,0 +1,2 @@
+package seedu.address.logic.parser.deliverable;public class FindCommandParser {
+}
---
+++ src/main/java/seedu/address/model/deliverable/deliverable/TitleDescriptionContainsKeywordsPredicate.java
@@ -0,0 +1,2 @@
+package seedu.address.model.deliverable.deliverable;public class TitleDescriptionContainsKeywordsPredicate {
+}
