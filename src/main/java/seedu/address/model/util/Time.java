1a7fc0151fcd6c35f6e28f75fcac791b58269fad
MerlinLim <merlinlim98@gmail.com>
2020-11-04 17:57:03 +0800

    Add util class Time and test cases to model
    Class Time takes in args and parses as hh:mm.

---
+++ src/main/java/seedu/address/model/util/Time.java
@@ -0,0 +1,87 @@
+package seedu.address.model.util;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Time implements Comparable<Time>{
+    public static final String TIME_REGEX = "(([0-1]\\d)|(2[0-3])):([0-5]\\d)";
+    public static final String MESSAGE_CONSTRAINTS =
+            "Time should be in the format of HH:mm, "
+                    + "minute must start with a leading zero.";
+
+    public static final String VALIDATION_REGEX = String.format("%s", TIME_REGEX);
+
+    public static final DateTimeFormatter TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("HH:mm");
+
+    public final LocalTime value;
+
+    /**
+     * Constructs a {@code time}.
+     *
+     * @param time A valid time.
+     */
+    public Time(String time) {
+        requireNonNull(time);
+
+        // Check for constraints
+        checkArgument(isValidTime(time), MESSAGE_CONSTRAINTS);
+
+        //Parse value
+        this.value = LocalTime.parse(time, TIME_FORMATTER);
+    }
+
+    /**
+     * Returns the LocalTime value object.
+     *
+     * @return the value of Time.
+     */
+    public LocalTime getLocalTime() {
+        return this.value;
+    }
+
+    /**
+     * Returns true if a given string is a valid Time.
+     *
+     * @param test string to test.
+     * @return result of match.
+     */
+    public static boolean isValidTime(String test) {
+        return isValidTimePattern(test);
+    }
+
+    /**
+     * Returns true if a given string is a valid Time.
+     *
+     * @param test string to test.
+     * @return result of match.
+     */
+    public static boolean isValidTimePattern(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public int compareTo(Time o) {
+        return this.value.compareTo(o.value);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Time) // instanceof handles nulls
+                && ((this.value.equals(((Time) other).value)));
+    }
+
+    @Override
+    public String toString() {
+        return this.value.format(TIME_FORMATTER);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
